2e575420b9bf40eae457bd2d025f52d94f5fb995
igeligel <kevinigeligeligel@gmail.com>
2016-06-15 22:54:25 +0200

    refactor(home)
    refactor whitespacing

--- src/vue-components/home.vue
+++ src/vue-components/home.vue
@@ -6,12 +6,22 @@
         <img class="activator" src="../assets/images/banner.png" />
       </div>
       <div class="card-content">
-        <span class="card-title activator grey-text text-darken-4">vue-materializecss-boilerplate<i class="material-icons right">more_vert</i></span>
-        <p><a>A starting point for creating projects with vue.js and materializecss.</a></p>
+        <span class="card-title activator grey-text text-darken-4">
+          vue-materializecss-boilerplate<i class="material-icons right">more_vert</i>
+        </span>
+        <p>
+          <a>
+            A starting point for creating projects with vue.js and materializecss.
+          </a>
+        </p>
       </div>
       <div class="card-reveal">
-        <span class="card-title grey-text text-darken-4">Fast instructions<i class="material-icons right">close</i></span>
-        <p>For full instructions see here: <a>Link</a></p>
+        <span class="card-title grey-text text-darken-4">
+          Fast instructions<i class="material-icons right">close</i>
+        </span>
+        <p>
+          For full instructions see here: <a>Link</a>
+        </p>
       </div>
     </div>
     </div>
