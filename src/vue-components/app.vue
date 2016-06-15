629a4d8e9cd5f3c90e82ed4d998fbff61714b1e1
igeligel <kevinigeligeligel@gmail.com>
2016-06-15 22:53:46 +0200

    style(app-component)
    fix tab (spaces)

--- src/vue-components/app.vue
+++ src/vue-components/app.vue
@@ -9,14 +9,15 @@
 </template>
 
 <style>
-  body {
-    display: flex;
-    min-height: 100vh;
-    flex-direction: column;
-  }
-  main {
-    flex: 1 0 auto;
-  }
+body {
+  display: flex;
+  min-height: 100vh;
+  flex-direction: column;
+}
+
+main {
+  flex: 1 0 auto;
+}
 </style>
 
 <script>
