c8005b997c8f55552bb17f651cd9d8ecbb248d5c
igeligel <kevinigeligeligel@gmail.com>
2016-06-23 13:42:51 +0200

    feat(collapsible)
    delete dropdown, add collapsible

--- src/js/collapsible.js
+++ src/js/collapsible.js
@@ -1,5 +1,4 @@
 $(document).ready(function(){
-  $('.collapsible').collapsible({
-    accordion : true // A setting that changes the collapsible behavior to expandable instead of the default accordion style
-  });
+  console.log('collapsible');
+  $('.collapsible').collapsible();
 });
